b7328267246387d6fc10437a87a9b5652978b414
RozanRamadani <114994099+RozanRamadani@users.noreply.github.com>
2025-08-17 08:29:51 +0700

    make a Tool

---
+++ app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources\PatientResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\CreateAction;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
+
+class TreatmentsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'treatments';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->minValue(0)
+                    ->maxLength(10),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('description')
+            ->columns([
+                TextColumn::make('description'),
+                TextColumn::make('price'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    
+}
