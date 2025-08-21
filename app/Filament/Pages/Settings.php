d5f25283ba859ab8853ac6c34dfc54f8e89fb2c0
RozanRamadani <114994099+RozanRamadani@users.noreply.github.com>
2025-08-21 19:23:50 +0700

    Custom page

---
+++ app/Filament/Pages/Settings.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Resources\OwnerResource;
+use App\Filament\Resources\PatientResource;
+use App\Filament\Resources\ToolResource;
+use Filament\Pages\Page;
+use Filament\Actions\Action;
+
+class Settings extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-cog-8-tooth';
+
+    protected static string $view = 'filament.pages.settings';
+
+    public static function canAccess(): bool
+    {
+        return auth()->user()->id == 1;
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('Tools')
+                ->action(fn() => redirect()->to(ToolResource::getUrl('index'))),
+            Action::make('Owners')
+                ->action(fn() => redirect()->to(OwnerResource::getUrl('index'))),
+            Action::make('Patients')
+                ->action(fn() => redirect()->to(PatientResource::getUrl('index'))),
+        ];
+    }
+}
